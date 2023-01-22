8ff5ad7e446f4bf440bff9115c90b6b623ac2beb
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-01-22 00:30:00 +0000

    complete code along

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,14 @@ import NavBar from './NavBar';
 
 function App(){
     return(
-        <BrowserRouter>
-        <NavBar/> 
-        <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/login" element={<Login />}/>
-        </Routes>
-        </BrowserRouter>
+        <div>
+            <NavBar/> 
+            <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+            <Route path="/login" element={<Login />}/>
+            </Routes>
+        </div>
     )
 }
 
